4495381df21100feb8c55c994fc7d37847545932
BTswap <69065610+btswap@users.noreply.github.com>
2020-09-28 21:55:08 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/btswap/rinkeby/precache-manifest.11f1f63b762b35f8d678fdc86cff18ee.js"
+  "https://cdn.jsdelivr.net/gh/btswap/rinkeby/precache-manifest.5785501b009d1bc58d13a4365d2825c7.js"
 );
 
 self.addEventListener('message', (event) => {
